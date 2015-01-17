53ed2423bce091a3b1b7f9b3e8e00e56eb9452ab
Suvir Jain <suvirj@gmail.com>
2015-01-16 17:06:21 -0800

    Added code to convert raw nxml files to  passages.

---
+++ PassageExtractor/Bio2Df.py
@@ -0,0 +1,138 @@
+__author__ = 'suvir'
+
+import os
+import re
+import pandas as pd
+
+
+class Bio2Df:
+    """
+    Class to convert files in BIO format to a dataframe
+    """
+
+    def __init__(self, dirname="."):
+        """
+        Constructor
+        :param dirname: directory with BIO format files
+        :return:
+        """
+        self.input_dir = dirname
+        self.window_size = 10
+        self.df = pd.DataFrame()
+
+    def parsePubmedId(self, filename):
+        try:
+            pubmed_id = re.findall("\d{8}", filename)[0]
+            return pubmed_id
+        except:
+            print "Error in parsing pubmed id", filename
+            return "123"
+            pass
+
+    def get_words_in_window(self, lines, start_idx, end_idx):
+        s = []
+        for idx in range(start_idx, end_idx):
+            try:
+                token = lines[idx].split()[0]
+                s.append(token)
+            except IndexError:
+                pass
+        return " ".join(s)
+
+    def get_lr_window(self, lines, lindex, rindex):
+        left_window = self.get_words_in_window(lines, lindex - self.window_size, lindex)
+        right_window = self.get_words_in_window(lines, rindex + 1, rindex + self.window_size)
+        return left_window, right_window
+
+    def parseLines(self, lines, filename):
+        # Parse BIO entities and return as a dataframe
+        multi_flag = False
+        multi_word_entity = []
+        list_of_list = []
+        last_tag_pair = ()
+        start_index = -1
+        end_index = -1
+
+        for idx in range(len(lines)):
+            l = lines[idx]
+            try:
+                token, tag1, tag2, label = l.split('\t')
+            except:
+                print "Error in parsing line", repr(l)
+                continue
+            #print token,tag1,tag2,label
+            if label.startswith('O') and multi_flag == True:
+                #First append the multi word entity
+                left_window, right_window = self.get_lr_window(lines, start_index, end_index)
+                list_of_list.append(
+                    [' '.join(multi_word_entity), 'Y', last_tag_pair[0], last_tag_pair[1], filename, left_window,
+                     right_window])
+                #Then append the current entity
+                list_of_list.append([token, 'N', tag1, tag2, filename, "NONE", "NONE"])
+                #Clear out all state variables
+                multi_flag = False
+                start_index = -1
+                end_index = -1
+                last_tag_pair = ()
+                multi_word_entity = []
+
+            elif label.startswith('I') and multi_flag == False:
+                raise ValueError("Error! I found without B")
+
+            elif label.startswith('I') and multi_flag == True:
+                multi_word_entity.append(token)
+                last_tag_pair = (tag1, tag2)
+                multi_flag = True
+                if end_index == -1:
+                    raise ValueError("Error! I found without B")
+                else:
+                    end_index = idx
+
+            elif label.startswith('B'):
+                multi_word_entity.append(token)
+                last_tag_pair = (tag1, tag2)
+                multi_flag = True
+                start_index = idx
+                end_index = idx
+
+            else:
+                #save multi_word_entity to df
+                list_of_list.append([token, 'N', tag1, tag2, filename, "NONE", "NONE"])
+                #Clear out all state variables
+                multi_flag = False
+                start_index = -1
+                end_index = -1
+                last_tag_pair = ()
+                multi_word_entity = []
+
+        #print len(list_of_list)
+        return pd.DataFrame(list_of_list, columns=['Entity', 'IsDiseaseTrait', 'Tag1', 'Tag2', 'PUBMEDID', 'LeftWindow',
+                                                   'RightWindow'])
+
+    def execute(self):
+        files = [f for f in os.listdir(self.input_dir) if not f.startswith(".")]
+        df_list = []
+        for f in files:
+            lines = open(self.input_dir + f).readlines()
+            # Parse pubmed id of the file
+            filename = self.parsePubmedId(f)
+            #Get all entities in the file in a single df
+            file_df = self.parseLines(lines, filename)
+            #All the dataframes in a single df
+            df_list.append(file_df)
+        # Combine all the files into a pandas dataframe
+        self.df = pd.concat(df_list, axis=0)
+        self.df = self.df[self.df.IsDiseaseTrait == 'Y']
+
+    def get_df(self):
+        print "Return dataframe made from BIO files"
+        print len(self.df)
+        return self.df
+
+
+if __name__ == '__main__':
+    runner = Bio2Df("../GWAS_data/nxml_bugfinding/bio/")
+    runner.execute()
+    runner.get_df()
+    print "Finished processing all files"
+
---
+++ PassageExtractor/Raw2Bio.py
@@ -0,0 +1,212 @@
+__author__ = 'suvir'
+
+import nltk, csv, os
+import xml.etree.ElementTree as ET
+from pytrie import SortedStringTrie as trie
+import token_feature_builder
+
+
+class Raw2Bio:
+    def __init__(self, dirname, dictpath, outputdir):
+        self.dictionary = trie()
+        self.input_dir = dirname  # Directory to look up files inside
+        self.label = "DT"  # DT = Disease Trait, ET = Ethnicity
+        self.input_mode = 'XML'
+        self.dictionary_path = dictpath
+        self.output_dir = outputdir
+        # List of all garbage tags
+        # garbage_tags = ['ref','xref','ref-list','ext-link','contrib','email','job','doi','base_name']
+        self.garbage_tags = []
+        self.parent_map = dict()
+        self.output_writer = None
+        self.debug_flag = False
+
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+
+    def build_features_for_training(self, root, text):
+        # Given an xml tag and its relevant text/tail portion, tokenize it and calculate features of each token
+        tokens = text.encode('ascii', 'ignore')
+        tokens = nltk.PunktWordTokenizer().tokenize(tokens)
+        match_flag = False
+        prev_token = ""
+        current_match = ""
+        current_match_tags = []
+
+        for this_token in tokens:
+            token = this_token.lower()
+            this_token = this_token.lower()
+            if match_flag:
+                """
+                Previous tokens have matched some pattern
+                """
+                prefix_string = current_match + " " + token
+                if len(self.dictionary.keys(prefix=prefix_string + " ")) > 0:
+                    """
+                    Running Match
+                    """
+                    current_match = current_match + " " + this_token
+                    current_match_tags.append(root)
+                    if self.debug_flag is True:
+                        print "2.", repr(current_match), ",", token, ",", root.tag
+                elif current_match in self.dictionary or prefix_string in self.dictionary:
+                    """
+                    Exact Match
+                    """
+                    current_match = current_match + " " + this_token
+                    current_match_tags.append(root)
+
+                    if self.debug_flag is True:
+                        print "3.", repr(current_match), ",", token, ",", root.tag
+
+                    tokens_to_write = current_match.split(' ')
+                    self.output_writer.writerow(
+                        token_feature_builder.features_for_EFO(current_match_tags[0], tokens_to_write[0]) + [
+                            "B-" + self.label])
+                    for i in range(1, len(tokens_to_write)):
+                        self.output_writer.writerow(
+                            token_feature_builder.features_for_EFO(current_match_tags[i], tokens_to_write[i]) + [
+                                "I-" + self.label])
+
+                    match_flag = False
+                    current_match_tags = []
+                    current_match = ""
+                else:
+                    if self.debug_flag is True:
+                        print "4.", repr(current_match), ",", repr(prefix_string), ",", token, ",", root.tag
+                    """
+                    The current_match was a good sequence. This token does not need to be added to the sequence.
+                    """
+
+                    tokens_to_write = current_match.split(' ')
+                    for i in range(len(tokens_to_write)):
+                        self.output_writer.writerow(
+                            token_feature_builder.features_for_EFO(current_match_tags[i], tokens_to_write[i]) + [
+                                "O-" + self.label])
+
+                    current_match_tags = []
+                    current_match = ""
+                    match_flag = False  # Running pattern has stopped
+
+            """
+            Previous tokens have not matched any pattern
+            """
+            if not match_flag:
+                if len(self.dictionary.keys(prefix=token + " ")) > 0:
+                    match_flag = True  # Running pattern has started
+                    current_match += this_token
+                    current_match_tags.append(root)
+                    if self.debug_flag is True:
+                        print "1.", token, ",", root.tag
+                elif token in self.dictionary:
+                    if self.debug_flag is True:
+                        print "5.", current_match, ",", token, ",", root.tag
+                    self.output_writer.writerow(
+                        token_feature_builder.features_for_EFO(root, this_token) + ["B-" + self.label])
+                else:
+                    if self.debug_flag is True:
+                        print "6.", current_match, ",", token, ",", root.tag
+                    self.output_writer.writerow(
+                        token_feature_builder.features_for_EFO(root, this_token) + ["O-" + self.label])
+
+        """
+        If code reaches here, it means that a pattern has been matched
+        and the process reached the end of text
+        """
+        if len(current_match) > 0:
+            if current_match in self.dictionary:
+                if self.debug_flag is True:
+                    print "7.", current_match, ",", token, ",", root.tag
+                tokens_to_write = current_match.split(' ')
+                self.output_writer.writerow(
+                    token_feature_builder.features_for_EFO(current_match_tags[0], tokens_to_write[0]) + [
+                        "B-" + self.label])
+                for i in range(1, len(tokens_to_write)):
+                    self.output_writer.writerow(
+                        token_feature_builder.features_for_EFO(current_match_tags[i], tokens_to_write[i]) + [
+                            "I-" + self.label])
+            else:
+                tokens_to_write = current_match.split(' ')
+                for i in range(len(tokens_to_write)):
+                    self.output_writer.writerow(
+                        token_feature_builder.features_for_EFO(current_match_tags[i], tokens_to_write[i]) + [
+                            "O-" + self.label])
+
+    def build_features_no_training(self, root, text):
+        """
+        Generate features for all tokens.
+        This is NOT for training - so there are no labels.
+        """
+        # Given an xml tag and its relevant text/tail portion, tokenize it and calculate features of each token
+        tokens = text.encode('ascii', 'ignore')
+        tokens = nltk.PunktWordTokenizer().tokenize(tokens)
+        for token in tokens:
+            self.output_writer.writerow(token_feature_builder.features_for_EFO(root, token))
+
+    def get_text_nodes(self, root):
+        if root.tag in self.garbage_tags:
+            return
+
+        if "type" in root.attrib:
+            if root.attrib["type"] in ["header", "footer"]:
+                return
+
+        if root.text and not root.text.startswith('\n'):
+            self.build_features_for_training(root, root.text)
+
+        if root.tail and not root.tail.startswith('\n'):
+            self.build_features_for_training(root, root.tail)
+
+        for child in root:
+            self.get_text_nodes(child)
+
+    def build_trie(self):
+        stopwords = nltk.corpus.stopwords.words('english')
+        t = dict()
+        f = open(self.dictionary_path)
+        l = f.readlines()
+        f.close()
+        line_number = 1
+        for line in l:
+            word = line.strip().lower()
+            if word not in stopwords:
+                t[word] = line_number
+                line_number += 1
+        self.dictionary.update(t)
+        print "Finished building Trie"
+
+    def execute(self):
+        self.build_trie()
+
+        # Choose the directory for training files
+        files = os.listdir(self.input_dir)
+
+        if self.input_mode == 'XML':
+            # Keep only XML files
+            files = [f for f in files if f.endswith('.xml')]
+
+            #Extract tokens and features from each file
+            for f in files:
+                #Open a CSV file to write in
+                output = open(self.output_dir + self.label + '_' + f, 'wb')
+                self.output_writer = csv.writer(output, delimiter='\t', quotechar='', escapechar='\\',
+                                                quoting=csv.QUOTE_NONE)
+                print "Now parsing " + f
+                #Parse XML for current file
+                tree = ET.parse(self.input_dir + f)
+                root = tree.getroot()
+                self.parent_map = dict()
+                self.parent_map = dict((c, p) for p in tree.getiterator() for c in p)
+                token_feature_builder.set_parent_map(self.parent_map)
+                self.get_text_nodes(root)
+
+                #Close file stream
+                output.close()
+
+
+if __name__ == '__main__':
+    runner = Raw2Bio("../GWAS_data/nxml_bugfinding/",
+                     "../GWAS_data/gazzetteers/efo_disease_traits",
+                     "../GWAS_data/nxml_bugfinding/bio/")
+    runner.execute()
+    print "Finished processing all files"
