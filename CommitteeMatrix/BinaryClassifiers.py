0ed1e8e6f8205bf27b724b7b5501929f8adf9bb7
Suvir Jain <suvirj@gmail.com>
2015-01-19 16:48:36 -0800

    Added code for some committee members.

--- CommitteeMatrix/BinaryClassifiers.py
+++ CommitteeMatrix/BinaryClassifiers.py
@@ -3,7 +3,7 @@ __author__ = 'suvir'
 
 class BinaryClassifiers(object):
     @staticmethod
-    def __in_title_abstract(self, row):
+    def __in_title_abstract(row):
         """
         Checks if the disease/trait mention appears in the title or abstract
         :param row: A row of a dataframe
@@ -12,7 +12,7 @@ class BinaryClassifiers(object):
         return row['tag'] in ['title', 'abstract']
 
     @staticmethod
-    def __exact_match(self, row, target):
+    def __exact_match(row, target):
         """
         Checks if the disease/trait mention is an exact match of the target.
         :param row: A row of a dataframe.
@@ -22,7 +22,7 @@ class BinaryClassifiers(object):
         return row['diseasetrait'] == target
 
     @staticmethod
-    def __partial_match(self, row, target):
+    def __partial_match(row, target):
         """
         Checks if the disease/trait mention is a partial match of the target.
         :param row: A row of a dataframe.
@@ -32,7 +32,7 @@ class BinaryClassifiers(object):
         return target.startswith(row['diseasetrait'])
 
     @staticmethod
-    def __is_synonym(self, row, synonyms):
+    def __is_synonym(row, synonyms):
         """
         Checks if the disease/trait mention is a synonym of the target.
         :param row: A row of a dataframe
@@ -41,3 +41,9 @@ class BinaryClassifiers(object):
         """
         return row['diseasetrait'] in synonyms
 
+
+    @staticmethod
+    def __has_multiple_tokens(row):
+        dt = row['diseasetrait']
+        assert len(dt) > 0, "Empty disease trait mention"
+        return not dt.isalpha()
